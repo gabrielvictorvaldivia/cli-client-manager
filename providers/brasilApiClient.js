e1e1af40ad9cfda96a6a195c7346ddf28fa041c3
Gabriel Valdivia <valdivia.gabrielvictor@gmail.com>
2026-04-16 00:24:21 -0300

    refactor: extrai função de busca de endereço para brasilApiClient
    - substitui `getAddress` por `getAddressByPostalCode`
    - melhora modularidade e organização do código

---
+++ providers/brasilApiClient.js
@@ -0,0 +1,16 @@
+export async function getAddressByPostalCode(postalCode) {
+    /*
+        {
+            "cep": "89010025",
+            "state": "SC",
+            "city": "Blumenau",
+            "neighborhood": "Centro",
+            "street": "Rua Doutor Luiz de Freitas Melro",
+            ...
+        }
+     */
+    const {data: {cep, state, city, neighborhood, street}} =
+        await axios.get(`https://brasilapi.com.br/api/cep/v2/${postalCode}`);
+
+    return {cep, state, city, neighborhood, street};
+}
