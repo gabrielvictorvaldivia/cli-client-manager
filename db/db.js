5b911fb4a08f65965aaa6ff3ee229b68ba5d316c
Gabriel Valdivia <valdivia.gabrielvictor@gmail.com>
2026-04-17 18:11:39 -0300

    feat: adiciona persistência de clientes com arquivo db.json
    - implementa gravação e leitura de clientes no arquivo db.json
    - ajusta função save para salvar clientes no arquivo
    - utiliza fs para gerenciar operações de leitura/escrita de dados

--- db/db.js
+++ db/db.js
@@ -1,16 +1,24 @@
-import {v4 as uuid} from 'uuid';
+import { v4 as uuid } from 'uuid';
+import fs from 'fs';
 
-const customers = [];
+const filePath = 'db.json';
 
 export function save(client) {
+    const customers = getAll();
+
     const id = uuid();
+    customers.push({ id, ...client });
 
-    customers.push({id, ...client});
+    fs.writeFileSync(filePath, JSON.stringify(customers, null, 2));
 
-    console.log(`Client registered successfully.`);
+    console.log('Client registered successfully.');
     return id;
 }
 
 export function getAll() {
-    return customers;
+    if (!fs.existsSync(filePath)) {
+        return [];
+    }
+    const data = fs.readFileSync(filePath, 'utf-8');
+    return JSON.parse(data);
 }
